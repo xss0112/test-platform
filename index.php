19a83bca8540427e2f8ad09972610db75b0b0dd1
xss112 <xss0112@126.com>
2015-08-18 18:43:26 +0800

    ..

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('APP_DEBUG', true);
-require '/opt/meituan/web/httpd-2.2.29/htdocs/ThinkPHP/ThinkPHP.php';
-?>
--- platform/index.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('APP_DEBUG', true);
-require '/Library/WebServer/Documents/ThinkPHP/ThinkPHP.php';
-?>
